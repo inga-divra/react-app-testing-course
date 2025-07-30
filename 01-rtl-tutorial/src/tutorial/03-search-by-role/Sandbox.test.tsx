91596f0c51d29d50c1159f6e7d7b6cbb59eb5912
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-07-30 19:55:04 +0300

    test: learn to query headings with getByRole using accessible name and level

--- 01-rtl-tutorial/src/tutorial/03-search-by-role/Sandbox.test.tsx
+++ 01-rtl-tutorial/src/tutorial/03-search-by-role/Sandbox.test.tsx
@@ -11,4 +11,10 @@ describe('03-search-by-role', () => {
     expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
   });
+
+  test('renders headings with correct hierarchy', () => {
+    render(<Sandbox />);
+    expect(screen.getByRole('heading', { name: 'Main Heading', level: 1 }));
+    expect(screen.getByRole('heading', { name: 'Subheading', level: 2 }));
+  });
 });
