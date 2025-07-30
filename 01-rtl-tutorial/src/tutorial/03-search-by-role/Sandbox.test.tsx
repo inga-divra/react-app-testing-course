eceeeacb38f285b71aba1014730d403387b116dc
inga-divra <106383378+inga-divra@users.noreply.github.com>
2025-07-30 20:07:36 +0300

    test: learned to query img elements by alt text and buttons by accessible name using getByRole

--- 01-rtl-tutorial/src/tutorial/03-search-by-role/Sandbox.test.tsx
+++ 01-rtl-tutorial/src/tutorial/03-search-by-role/Sandbox.test.tsx
@@ -7,7 +7,6 @@ describe('03-search-by-role', () => {
     const { container } = render(<Sandbox />);
     logRoles(container);
     expect(screen.getByRole('navigation')).toBeInTheDocument();
-
     expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
   });
@@ -16,5 +15,19 @@ describe('03-search-by-role', () => {
     render(<Sandbox />);
     expect(screen.getByRole('heading', { name: 'Main Heading', level: 1 }));
     expect(screen.getByRole('heading', { name: 'Subheading', level: 2 }));
+  });
+
+  test('renders image with alt text', () => {
+    render(<Sandbox />);
+    expect(screen.getByRole('img', { name: 'Example' })).toBeInTheDocument();
+  });
+
+  test('renders initial buttons', () => {
+    render(<Sandbox />);
+    expect(
+      screen.getByRole('button', { name: 'Click me' })
+    ).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Submit' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Cancel' })).toBeInTheDocument();
   });
 });
